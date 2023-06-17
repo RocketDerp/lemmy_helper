239dc14a2888f895641d6448ae385dc9ed146a68
RocketDerp <RoundSparrow@GMail.com>
2023-06-17 13:37:46 -0700

    html table

--- src/routes/query/[name]/+page.svelte
+++ src/routes/query/[name]/+page.svelte
@@ -6,14 +6,47 @@
 
 <h1>Lemmy-Helper Query: {data.queryName}</h1>
 
-output goes here
+Database result row count: {data.outRowsRaw.count}
 
 <pre>
     {data.outRows}
 </pre>
+
+<hr />
 
 <nl>
 {#each data.outRowsRaw as singleRow}
     <li><pre>{JSON.stringify(singleRow)}</pre></li>
 {/each}
 </nl>
+
+<hr />
+
+<table>
+  <thead>
+    <tr>
+      {#each Object.keys(data.outRowsRaw[0]) as columnHeading}
+        <th>{columnHeading}</th>
+      {/each}
+    <tr/>
+  </thead>
+
+  <tbody>
+    {#each Object.values(data.outRowsRaw) as row}
+      <tr>
+        {#each Object.values(row) as cell}
+          <td>{cell}</td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+
+<style>
+    table, th, td {
+      border: 1px solid;
+      border-collapse: collapse;
+      margin: 10px;
+    }
+</style>
