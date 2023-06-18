8339ca5d7762991476e82e9432b79fe09e02eb24
RocketDerp <RoundSparrow@GMail.com>
2023-06-18 10:49:24 -0700

    pg statements

--- src/routes/query/[name]/+page.server.ts
+++ src/routes/query/[name]/+page.server.ts
@@ -31,6 +31,11 @@ export const load: PageServerLoad = async (incoming) => {
 			ORDER BY query_start desc
 			;`
 			break;
+		case 'pgstatements':
+			// https://www.timescale.com/blog/identify-postgresql-performance-bottlenecks-with-pg_stat_statements/
+			sqlQuery = `SELECT * FROM pg_stat_statements
+			;`
+			break;
 		case 'explain_posts':
 			sqlQuery = `EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
 			 SELECT post.id, post.name, post.url, post.body, post.creator_id, post.community_id, post.removed, post.locked, post.published, post.updated, post.deleted, post.nsfw, post.embed_title, post.embed_description, post.embed_video_url, post.thumbnail_url, post.ap_id, post.local, post.language_id, post.featured_community, post.featured_local,
