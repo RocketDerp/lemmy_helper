28bdae9f8d8b502ebb2281501da872e4a5efd569
RocketDerp <RoundSparrow@GMail.com>
2023-06-18 07:46:22 -0700

    federated count

--- src/routes/query/[name]/+page.server.ts
+++ src/routes/query/[name]/+page.server.ts
@@ -62,7 +62,7 @@ export const load: PageServerLoad = async (incoming) => {
 				GROUP BY
 					community_id
 			) a INNER JOIN community c on c.id = a.community_id
-			ORDER BY c.published DESC
+			ORDER BY c.post_count DESC
 			;`
 			break;
 		case 'federatedcommentcount':
@@ -79,7 +79,7 @@ export const load: PageServerLoad = async (incoming) => {
 				GROUP BY
 					post_id
 			) a INNER JOIN post c on c.id = a.post_id
-			ORDER BY c.published DESC
+			ORDER BY a.comment_count DESC
 			;`
 			break;
 		case 'posts':
