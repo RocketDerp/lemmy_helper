d745b872d2d5a80f90587fe0807c6cabe90899c8
RocketDerp <RoundSparrow@GMail.com>
2023-06-18 09:38:35 -0700

    raw person

--- src/routes/query/[name]/+page.server.ts
+++ src/routes/query/[name]/+page.server.ts
@@ -99,8 +99,9 @@ export const load: PageServerLoad = async (incoming) => {
 			;`
 			break;
 		case 'raw_person':
-			sqlQuery = `SELECT id, *
+			sqlQuery = `SELECT id, actor_id, name, display_name, published, local, instance_id, matrox_user_id
 			FROM person
+			ORDER BY published DESC
 			LIMIT 10
 			;`
 			break;
