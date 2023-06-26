261200b544e2f2ac3137b7142104a864dbd9d568
RocketDerp <RoundSparrow@GMail.com>
2023-06-25 18:03:50 -0700

    move code to library

--- src/lib/lemmy_sort.js
+++ src/lib/lemmy_sort.js
@@ -1,3 +1,58 @@
+export function parseHrtimeToSeconds(hrtime) {
+    var seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
+    return seconds;
+}
+
+
+export async function getLemmyPosts(params0, fetch) {
+	let result0 = {};
+	const startTime = process.hrtime();
+	let serverURL0 = params0.serverChoice0 + params0.serverAPI0;
+	let resp = await fetch(serverURL0);
+	result0.timeConnect = parseHrtimeToSeconds(process.hrtime(startTime));
+	if (resp.ok) {
+		const queryTimeStart = process.hrtime();
+		try {
+			result0.json = await resp.json();
+		} catch (e0) {
+			result0.json = [];
+			result0.failureCode = -1000;
+			result0.failureText = "JSON parse failure";
+		}
+		result0.timeParse = parseHrtimeToSeconds(process.hrtime(queryTimeStart))
+		// console.log(result0.json);
+	} else {
+		result0.json = [];
+		result0.failureCode = resp.status;
+		result0.failureText = resp.statusText;
+	}
+
+	return result0;
+}
+
+
+export async function dualServerPostFetch(results) {
+    if (1==1) {
+		results.server0params = { serverChoice0: "https://lemmy.world/",
+		    serverAPI0: "api/v3/post/list?sort=New&community_name=asklemmy@lemmy.ml&limit=50&page=1",
+		}
+		    //serverAPI0: "api/v3/post/list?sort=New&type_=Local&limit=40&page=1",
+			// Today I learned, til 16791
+		    //serverAPI0 = "api/v3/post/list?sort=New&community_id=16791&limit=50page=1"
+		results.outServer0 = await getLemmyPosts(results.server0params, fetch);
+	};
+
+	if (1==1) {
+		results.server1params = { serverChoice0: "https://lemmy.ml/",
+		    serverAPI0: "api/v3/post/list?sort=New&community_name=asklemmy@lemmy.ml&limit=50&page=1",
+		}
+		results.outServer1 = await getLemmyPosts(results.server1params, fetch);
+	};
+
+    return results;
+}
+
+
 // both posts arrays should be sorted by published timestamp
 export function matchPosts(posts0, posts1) {
     let results = {
--- src/routes/lemmy-replication/+page.server.ts
+++ src/routes/lemmy-replication/+page.server.ts
@@ -1,31 +1,5 @@
 import type { PageServerLoad } from './$types'
-
-
-async function getLemmyPosts(params0) {
-	let result0 = {};
-	const startTime = process.hrtime();
-	let serverURL0 = params0.serverChoice0 + params0.serverAPI0;
-	let resp = await fetch(serverURL0);
-	result0.timeConnect = parseHrtimeToSeconds(process.hrtime(startTime));
-	if (resp.ok) {
-		const queryTimeStart = process.hrtime();
-		try {
-			result0.json = await resp.json();
-		} catch (e0) {
-			result0.json = [];
-			result0.failureCode = -1000;
-			result0.failureText = "JSON parse failure";
-		}
-		result0.timeParse = parseHrtimeToSeconds(process.hrtime(queryTimeStart))
-		// console.log(result0.json);
-	} else {
-		result0.json = [];
-		result0.failureCode = resp.status;
-		result0.failureText = resp.statusText;
-	}
-
-	return result0;
-}
+import { dualServerPostFetch } from '$lib/lemmy_sort'
 
 
 export const load: PageServerLoad = async (incoming) => {
@@ -46,29 +20,8 @@ export const load: PageServerLoad = async (incoming) => {
 	};
 
 
-    if (1==1) {
-		results.server0params = { serverChoice0: "https://lemmy.world/",
-		    serverAPI0: "api/v3/post/list?sort=New&community_name=asklemmy@lemmy.ml&limit=50&page=1",
-		}
-		    //serverAPI0: "api/v3/post/list?sort=New&type_=Local&limit=40&page=1",
-			// Today I learned, til 16791
-		    //serverAPI0 = "api/v3/post/list?sort=New&community_id=16791&limit=50page=1"
-		results.outServer0 = await getLemmyPosts(results.server0params);
-	};
-
-	if (1==1) {
-		results.server1params = { serverChoice0: "https://lemmy.ml/",
-		    serverAPI0: "api/v3/post/list?sort=New&community_name=asklemmy@lemmy.ml&limit=50&page=1",
-		}
-		results.outServer1 = await getLemmyPosts(results.server1params);
-	};
+	results = await dualServerPostFetch(results);
 
 
 	return results;
-}
-
-
-function parseHrtimeToSeconds(hrtime) {
-    var seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
-    return seconds;
 }
