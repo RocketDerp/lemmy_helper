5b60f6fff41aaaa138baac02634b354370114262
RocketDerp <RoundSparrow@GMail.com>
2023-06-25 19:02:45 -0700

    console output of posts

--- commands/posts.js
+++ commands/posts.js
@@ -1,8 +1,10 @@
 import { dualServerPostFetch, matchPosts } from "../src/lib/lemmy_sort.js"
 
+
 function showPerf(results) {
     console.log("timeConnect %d timeParse %d server %s", results.timeConnect, results.timeParse, results.params0.serverChoice0);
 }
+
 
 export async function posts () {
     console.log("posts");
@@ -15,4 +17,14 @@ export async function posts () {
     let matchResults = matchPosts(results.outServer0.json.posts, results.outServer1.json.posts);
 
     console.log(matchResults.resultsA);
+    consolePosts(matchResults.unfoundA);
+
+}
+
+
+function consolePosts(postArray) {
+    for (let i = 0; i < postArray.length; i++) {
+        let post = postArray[i].post;
+        console.log("%d %s %s url %s", post.id, post.published, post.name, post.url);
+    }
 }
