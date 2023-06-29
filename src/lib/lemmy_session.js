97613da273df24166373c45dfa42a14963ad1d4c
RocketDerp <RoundSparrow@GMail.com>
2023-06-29 14:25:13 -0700

    function consolidation

--- src/lib/lemmy_session.js
+++ src/lib/lemmy_session.js
@@ -17,65 +17,20 @@ function parseJwt (token) {
 }
 
 
-export async function lemmyLogin(params0, fetcha) {
+export async function serverFetchJSON0(params0, fetcha) {
 	let result0 = { params0: params0,
         failureCode: -1,
         failureText: "",
         json: {}
         };
-	let serverURL0 = params0.serverChoice0 + "api/v3/user/login";
-	const startTime = process.hrtime();
-    let resp = await fetch(serverURL0, {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify( {
-            username_or_email: params0.usernameOrEmail,
-            password: params0.password
-            } )
-        });
+	let serverURL0 = params0.serverChoice0 + params0.serverURLpath0;
 
-	result0.timeConnect = parseHrtimeToSeconds(process.hrtime(startTime));
-	if (resp.ok) {
-		const queryTimeStart = process.hrtime();
-		try {
-			result0.json = await resp.json();
-	    	// console.log(result0.json);
-            if (result0.json.jwt) {
-                result0.auth = parseJwt(result0.json.jwt);
-            }
-        } catch (e0) {
-            console.error("JSON parse failed ", serverURL0);
-			result0.failureCode = -1000;
-			result0.failureText = "JSON parse failure";
-		}
-		result0.timeParse = parseHrtimeToSeconds(process.hrtime(queryTimeStart))
-	} else {
-        console.error("fetch failed ", serverURL0);
-		result0.failureCode = resp.status;
-		result0.failureText = resp.statusText;
-	}
-
-	return result0;
-}
-
-
-export async function lemmyCommentLike(params0, fetcha) {
-	let result0 = { params0: params0,
-        failureCode: -1,
-        failureText: "",
-        json: {}
-        };
-	let serverURL0 = params0.serverChoice0 + "api/v3/comment/like";
 	const startTime = process.hrtime();
     try {
         let resp = await fetch(serverURL0, {
             method: "POST",
             headers: { "content-type": "application/json" },
-            body: JSON.stringify( {
-                comment_id: params0.comment_id,
-                score: 1,
-                auth: params0.jwt
-                } )
+            body: params0.bodyJSON0
             });
         result0.timeConnect = parseHrtimeToSeconds(process.hrtime(startTime));
         if (resp.ok) {
@@ -85,19 +40,52 @@ export async function lemmyCommentLike(params0, fetcha) {
                 // console.log(result0.json);
             } catch (e0) {
                 console.error("JSON parse failed ", serverURL0);
+                console.log(e0);
                 result0.failureCode = -1000;
                 result0.failureText = "JSON parse failure";
             }
             result0.timeParse = parseHrtimeToSeconds(process.hrtime(queryTimeStart))
         } else {
-            console.error("fetch failed ", serverURL0);
+            console.error("JSON exception ", serverURL0);
             result0.failureCode = resp.status;
             result0.failureText = resp.statusText;
         }
     } catch (err) {
+        console.error("fetch exception ", serverURL0);
+        console.log(err);
         result0.failureCode = -2000;
         result0.failureText = err;
     }
 
 	return result0;
+}
+
+
+export async function lemmyLogin(params0, fetcha) {
+    params0.serverURLpath0 = "api/v3/user/login";
+    params0.bodyJSON0 = JSON.stringify( {
+        username_or_email: params0.usernameOrEmail,
+        password: params0.password
+        } );
+
+    let result0 = serverFetchJSON0(params0, fetcha);
+    if (result0.failureCoee == -1) {
+        if (result0.json.jwt) {
+            result0.auth = parseJwt(result0.json.jwt);
+        }
+    }
+    return result0;
+}
+
+
+export async function lemmyCommentLike(params0, fetcha) {
+    params0.serverURLpath0 = "api/v3/comment/like";
+    params0.bodyJSON0 = JSON.stringify( {
+        comment_id: params0.comment_id,
+        score: 1,
+        auth: params0.jwt
+        } );
+
+    let result0 = serverFetchJSON0(params0, fetcha);
+    return result0;
 }
